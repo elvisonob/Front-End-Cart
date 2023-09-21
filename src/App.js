8b6fa00532445ddf37c2825ed42cc6fe35f7dac9
elvisonob <elvisonob@yahoo.com>
2023-09-21 04:52:13 +0100

    more functionalities

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import './App.css';
 import Basket from './components/Basket';
 import Header from './components/Header';
 import Main from './components/Main';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
   return (
     <div className="App">
       <Header></Header>
       <div className="row">
-        <Main></Main>
-        <Basket></Basket>
+        <Main products={products}></Main>
+        <Basket cartItems={cartItems}></Basket>
       </div>
     </div>
   );
 }
 
 export default App;
+
+// const onClickAddCart = () => {
+//   setAddedItem(
+//     addItemToCart.map((foodItems) => (
+//       <Cart id={foodItems.id} name={foodItems.name} />
+//     ))
+//   );
+// };
