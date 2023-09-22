6adfdbad5c88a42c61987cdc0a56c739aac48dfe
elvisonob <elvisonob@yahoo.com>
2023-09-22 04:42:21 +0100

    cart implementation sorted

--- src/App.js
+++ src/App.js
@@ -7,23 +7,47 @@ import { useState } from 'react';
 function App() {
   const { products } = data;
   const [cartItems, setCartItems] = useState([]);
+
+  const onAddToCart = (product) => {
+    const existingItem = cartItems.find((cart) => cart.id === product.id);
+    if (existingItem) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id
+            ? { ...existingItem, qty: existingItem.qty + 1 }
+            : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+
+  const onRemoveFromCart = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
       <div className="row">
-        <Main products={products}></Main>
-        <Basket cartItems={cartItems}></Basket>
+        <Main onAdd={onAddToCart} products={products} />
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAddToCart}
+          onRemove={onRemoveFromCart}
+        />
       </div>
     </div>
   );
 }
 
 export default App;
-
-// const onClickAddCart = () => {
-//   setAddedItem(
-//     addItemToCart.map((foodItems) => (
-//       <Cart id={foodItems.id} name={foodItems.name} />
-//     ))
-//   );
-// };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Product = (props) => {
-  const { product } = props;
+  const { product, onAddItem } = props;
   return (
     <div>
       <img className="small" src={product.image} alt={product.name}></img>
       <h3>{product.name}</h3>
       <div>${product.price}</div>
       <div>
-        <button>Add To Cart</button>
+        <button onClick={() => onAddItem(product)}>Add To Cart</button>
       </div>
     </div>
   );
